dce3f391a855b99b6ee31a2f27a1292b6361df70
ingaSiu <ingasiud@gmail.com>
2026-02-27 07:04:08 +0200

    refactor: add error message

--- day-01/container-presenter-pattern/src/with-pattern/components/UserProfilePresenter.jsx
+++ day-01/container-presenter-pattern/src/with-pattern/components/UserProfilePresenter.jsx
@@ -1,10 +1,14 @@
+import ErrorMessage from './common/ErrorMessage';
 import LoadingSpinner from './common/LoadingSpinner';
 
-const UserProfilePresenter = ({ user, posts, loading, error }) => {
+const UserProfilePresenter = ({ user, posts, loading, error, onRetry }) => {
   if (loading) {
     return <LoadingSpinner message="Loading user profile..." />;
   }
 
+  if (error) {
+    return <ErrorMessage title="Oops! Something went wrong" message={error} onRetry={onRetry} />;
+  }
   return <div>UserProfilePresenter</div>;
 };
 
