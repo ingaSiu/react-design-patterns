6188834e8795b1fe7753d41d3f493980105f4b94
ingaSiu <ingasiud@gmail.com>
2026-03-01 07:22:17 +0200

    task:  add main components with TODO

---
+++ day-01/container-presenter-pattern/src/with-pattern/components/task/ProductListContainer.jsx
@@ -0,0 +1,9 @@
+const ProductListContainer = () => {
+  // TODO:
+  // API calls and data fetching
+  // Cart management
+  // Error handling
+  return <div>ProductListContainer</div>;
+};
+
+export default ProductListContainer;
---
+++ day-01/container-presenter-pattern/src/with-pattern/components/task/ProductListPresenter.jsx
@@ -0,0 +1,14 @@
+// Break down into smaller presenters:
+// ProductCard
+// SortFilterControls
+// CartSummary
+
+const ProductListPresenter = () => {
+  // TODO:
+  // Rendering products
+  // Sort/filter UI interactions
+  // Add to cart button clicks
+  return <div>ProductListPresenter</div>;
+};
+
+export default ProductListPresenter;
