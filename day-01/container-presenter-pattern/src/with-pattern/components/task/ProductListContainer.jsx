192ce09bf047ed3c00de35f7b7d05bfa58d0e819
ingaSiu <ingasiud@gmail.com>
2026-03-01 15:34:09 +0200

    task: add fetch products functionality

--- day-01/container-presenter-pattern/src/with-pattern/components/task/ProductListContainer.jsx
+++ day-01/container-presenter-pattern/src/with-pattern/components/task/ProductListContainer.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import ProductListPresenter from './ProductListPresenter';
+import axios from 'axios';
+
 const ProductListContainer = () => {
   // TODO:
-  // API calls and data fetching
+  // API calls and data fetching:
+  // - GET all products +
+  // - GET filtered products
   // Cart management
   // Error handling
-  return <div>ProductListContainer</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/api/products`);
+      console.log(response.data);
+      setProducts(response.data);
+    } catch {
+      console.log('Something went wrong');
+    }
+  };
+  return <ProductListPresenter products={products} />;
 };
 
 export default ProductListContainer;
