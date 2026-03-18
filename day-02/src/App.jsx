eaf3e535d6d7e76d51d0639d61ab458566fb3f5b
ingaSiu <ingasiud@gmail.com>
2026-03-18 06:42:10 +0200

    import component

--- day-02/src/App.jsx
+++ day-02/src/App.jsx
@@ -1,9 +1,11 @@
+import AutoFocusInput from './state-ref/components/AutoFocusInput';
 import Counter from './state-ref/components/Counter';
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <Counter />
+      <AutoFocusInput />
     </div>
   );
 }
