aed5e3bc157319841284139c9bde89886941f3ad
ingaSiu <ingasiud@gmail.com>
2026-03-20 08:08:57 +0200

    refactor feedback form to controlled

---
+++ day-02/src/controlled/ControlledFeedbackForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+export default function ControlledFeedbackForm() {
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+
+  // handle the values change
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Form submitted:', form);
+  };
+
+  return (
+    <form className="flex flex-col" onSubmit={handleSubmit}>
+      <input
+        className="border rounded-2xl p-2 my-3"
+        type="text"
+        value={form.name}
+        onChange={handleChange}
+        placeholder="Name"
+      />
+      <input
+        className="border rounded-2xl p-2 my-3"
+        type="email"
+        value={form.email}
+        onChange={handleChange}
+        placeholder="Email"
+      />
+      <textarea
+        className="border rounded-2xl p-2 my-3"
+        value={form.message}
+        onChange={handleChange}
+        placeholder="Your message"
+      />
+      <button className="bg-purple-500 text-white p-1 rounded" type="submit">
+        Send Feedback
+      </button>
+    </form>
+  );
+}
