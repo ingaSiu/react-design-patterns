0b6e628a5c5044d3e8fddac2d99836c296a0b80e
ingaSiu <ingasiud@gmail.com>
2026-03-20 07:33:52 +0200

    add explanation for the component functionality

--- day-02/src/state-ref/components/CounterWithRef.jsx
+++ day-02/src/state-ref/components/CounterWithRef.jsx
@@ -4,6 +4,11 @@ function CounterWithRef() {
   const countRef = useRef(0); // persists between renders
   const [renderCount, setRenderCount] = useState(0); // for forcing re-renders
 
+  // then clicking on ref btn you will see incremented numbers in console
+  // but only then we click on the state btn these numbers will appear in UI, because they
+  // are persisted between renderings and only state component calls render
+  // so clicking ref btn 5 times and state btn 1 time (calls re-render)
+  // so ref would be 5 and state would be 1
   const increment = () => {
     countRef.current = countRef.current + 1; // update ref
     console.log('Ref Count:', countRef.current);
