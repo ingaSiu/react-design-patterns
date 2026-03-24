007226dd33b693dbc8ad13ec77cb49416b40693c
ingaSiu <ingasiud@gmail.com>
2026-03-24 06:28:47 +0200

    use messy modal with props

--- day-03/src/App.jsx
+++ day-03/src/App.jsx
@@ -1,14 +1,16 @@
+import './App.css';
+
+import Modal from './messy/Modal';
+
 function App() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
-      <p className="bg-gray-300 p-2 rounded-sm text-3xl">
-        <a 
-          href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
-          target="_blank"
-          className="underline mx-1">Learn React 19 With Code</a>
-        <span>▶️</span>  
-      </p>
+      <Modal
+        title="Delete Acount"
+        body="Are you sure you want to delete your account?"
+        primaryAction={<button>Delete</button>}
+        secondaryAction={<button>Cancel</button>}
+      />
     </div>
   );
 }
