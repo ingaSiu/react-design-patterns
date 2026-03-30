5b8c81e128c422030773b1e03b5e67bcffe08110
ingaSiu <ingasiud@gmail.com>
2026-03-30 19:38:08 +0300

    add button for opening modal, text-color

--- day-03/src/App.jsx
+++ day-03/src/App.jsx
@@ -6,17 +6,24 @@ import { useState } from 'react';
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center mt-3">
+      <button onClick={() => setIsOpen(true)} className="cursor-pointer">
+        Open Modal
+      </button>
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <Modal.Header>
-          <h2>Welcome!</h2>
+          <h2 className="text-black">Welcome!</h2>
         </Modal.Header>
         <Modal.Body>
-          <p>This is a modal built with the Compound Component pattern.</p>
+          <p className="text-black">This is a modal built with the Compound Component pattern.</p>
         </Modal.Body>
         <Modal.Footer>
-          <button onClick={() => setIsOpen(false)}>Close</button>
-          <button onClick={() => alert('Action performed!')}>Do Action</button>
+          <button className="text-black" onClick={() => setIsOpen(false)}>
+            Close
+          </button>
+          <button className="text-black" onClick={() => alert('Action performed!')}>
+            Do Action
+          </button>
         </Modal.Footer>
       </Modal>
     </div>
