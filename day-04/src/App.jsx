77228887152a75cf56ce0c39a2e9ef818001d6e6
ingaSiu <ingasiud@gmail.com>
2026-03-31 07:18:29 +0300

    use MouseTracker component with render props pattern

--- day-04/src/App.jsx
+++ day-04/src/App.jsx
@@ -1,11 +1,28 @@
-import BikeTracker from './messy/BikeTracker';
-import CarTracker from './messy/CarTracker';
+// import BikeTracker from './messy/BikeTracker';
+// import CarTracker from './messy/CarTracker';
+import MouseTracker from './with-pattern/MouseTracker';
 
 function App() {
   return (
     <div className="flex flex-col items-center">
-      <CarTracker />
-      <BikeTracker />
+      {/* <CarTracker />
+      <BikeTracker /> */}
+
+      <MouseTracker
+        render={(pos) => (
+          <p>
+            🚗 Car is at ({pos.x}, {pos.y})
+          </p>
+        )}
+      />
+
+      <MouseTracker
+        render={({ x, y }) => (
+          <p>
+            🏍️ Bike is at ({x}, {y})
+          </p>
+        )}
+      />
     </div>
   );
 }
