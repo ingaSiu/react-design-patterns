3754e4f072d12639db1e3902709dfb5ce27bdc0a
ingaSiu <ingasiud@gmail.com>
2026-04-10 07:49:45 +0300

    create useFetch custom hook

--- day-06/src/App.jsx
+++ day-06/src/App.jsx
@@ -1,9 +1,11 @@
+import MoviesList from './components/MovieList';
 import ThemeSwitcher from './components/ThemeSwitcher';
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       <ThemeSwitcher />
+      <MoviesList />
     </div>
   );
 }
