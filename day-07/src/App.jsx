cd13a53e497363d35dcea4b95498eefc5fd897c1
ingaSiu <ingasiud@gmail.com>
2026-04-11 07:19:17 +0300

    step 5: use data from context

--- day-07/src/App.jsx
+++ day-07/src/App.jsx
@@ -1,5 +1,25 @@
+import { useTheme } from './hook/useTheme';
+
 function App() {
-  return <div className="flex flex-col items-center"></div>;
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div
+      style={{
+        backgroundColor: theme ? '#fff' : '#222',
+        color: theme ? '#000' : '#fff',
+        height: '100vh',
+      }}
+    >
+      <nav className="flex justify-between bg-slate-500 p-1">
+        <h1 className="text-3xl">My App</h1>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+      </nav>
+      <main className="p-4 text-center">
+        <p className="text-xl m-3">{theme ? '☀️ Light Mode' : '🌙 Dark Mode'}</p>
+      </main>
+    </div>
+  );
 }
 
 export default App;
