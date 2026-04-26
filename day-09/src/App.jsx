92d9a2efa01c8e7811e83a2cba04d52c7735e605
ingaSiu <ingasiud@gmail.com>
2026-04-26 18:30:16 +0300

    form fiels with default provider

--- day-09/src/App.jsx
+++ day-09/src/App.jsx
@@ -1,3 +1,5 @@
+import FormFields from './components/FormField';
+import { FormProvider } from './provider/FormProvider';
 import Toggle from './components/Toggle';
 import { customToggleReducer } from './reducers/toggle-reducer';
 
@@ -6,6 +8,9 @@ function App() {
     <div className="flex flex-col items-center">
       <Toggle />
       <Toggle reducer={customToggleReducer} />
+      <FormProvider>
+        <FormFields />
+      </FormProvider>
     </div>
   );
 }
