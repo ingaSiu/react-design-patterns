06dab87042a2b605d644120ae9b0ee54562287c2
ingaSiu <ingasiud@gmail.com>
2026-04-26 18:30:46 +0300

    form provider with custom reducer

--- day-09/src/App.jsx
+++ day-09/src/App.jsx
@@ -1,6 +1,7 @@
 import FormFields from './components/FormField';
 import { FormProvider } from './provider/FormProvider';
 import Toggle from './components/Toggle';
+import { customFormReducer } from './reducers/form-reducer';
 import { customToggleReducer } from './reducers/toggle-reducer';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="flex flex-col items-center">
       <Toggle />
       <Toggle reducer={customToggleReducer} />
-      <FormProvider>
+      <FormProvider reducer={customFormReducer}>
         <FormFields />
       </FormProvider>
     </div>
