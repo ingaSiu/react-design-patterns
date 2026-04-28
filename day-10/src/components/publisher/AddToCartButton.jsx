93d2a8e7142e5bb697da6137239395e2115214e4
ingaSiu <ingasiud@gmail.com>
2026-04-28 07:25:48 +0300

    add explanation

--- day-10/src/components/publisher/AddToCartButton.jsx
+++ day-10/src/components/publisher/AddToCartButton.jsx
@@ -8,6 +8,14 @@ const products = [
   { id: crypto.randomUUID(), name: 'Light' },
 ];
 
+// whenever this button gets clicked, onClick this button will pick up one of the product randomly
+// from these products array and that particular product detail will be published
+// on the eventBus on this particular event
+
+// in eventBus is a Map and it will be populated with the event called 'cart:add'
+// and a listener set inside which whoever going to subscribe the will get added
+// that only will happen when I call the subscribe method
+
 export default function AddToCartButton() {
   const handleClick = () => {
     const randomIndex = Math.floor(Math.random() * products.length);
