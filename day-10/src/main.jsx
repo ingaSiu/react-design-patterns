2788fea66f3552ddcbcc22da65c156d41c75386e
ingaSiu <ingasiud@gmail.com>
2026-04-30 07:50:08 +0300

    attach listener to application

--- day-10/src/main.jsx
+++ day-10/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+import './lib/listener.js'; // attach listener to your application
+
+import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
