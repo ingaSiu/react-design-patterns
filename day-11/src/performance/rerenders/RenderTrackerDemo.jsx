b1d7923d00d8291693117e17df62be09be72ffcb
ingaSiu <ingasiud@gmail.com>
2026-05-04 07:08:54 +0300

    add render demo example: children re-rendering after parent component changes

---
+++ day-11/src/performance/rerenders/RenderTrackerDemo.jsx
@@ -0,0 +1,15 @@
+import RenderTracker from './RenderTracker';
+import { useState } from 'react';
+
+const RenderTrackerDemo = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <div className="p-2 border rounded">
+      <RenderTracker />
+      <input className="border rounded p-1" value={value} onChange={(e) => setValue(e.target.value)} />
+    </div>
+  );
+};
+
+export default RenderTrackerDemo;
