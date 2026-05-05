e6e58feb7b27cdbdee00d8e34f909833c4f0152c
ingaSiu <ingasiud@gmail.com>
2026-05-05 07:08:46 +0300

    useCallback demo

---
+++ day-11/src/performance/memoization/useCallback/Child.jsx
@@ -0,0 +1,8 @@
+import { memo } from 'react';
+
+const Child = memo(function Child({ onClick }) {
+  console.log('Child Rendered');
+  return <button onClick={onClick}>Click Me</button>;
+});
+
+export default Child;
---
+++ day-11/src/performance/memoization/useCallback/ChildDemo.jsx
@@ -0,0 +1,27 @@
+import { useCallback, useState } from 'react';
+
+import Child from './Child';
+
+const ChildDemo = () => {
+  const [count, setCount] = useState(0);
+
+  // memoize handleClick with useCallback
+  const handleClick = useCallback(() => {
+    console.log('Child Clicked!');
+  }, []);
+
+  console.log('App Rendered');
+
+  return (
+    <div className="p-2 border rounded">
+      <p>Count: {count}</p>
+      <div className="space-x-2">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+
+        <Child onClick={handleClick} />
+      </div>
+    </div>
+  );
+};
+
+export default ChildDemo;
