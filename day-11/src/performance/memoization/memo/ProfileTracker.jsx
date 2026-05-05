b0a3a2f365809e70aa9a234aec9dd6d6218723c0
ingaSiu <ingasiud@gmail.com>
2026-05-05 07:08:33 +0300

    fix spelling

--- day-11/src/performance/memoization/memo/ProfileTracker.jsx
+++ day-11/src/performance/memoization/memo/ProfileTracker.jsx
@@ -1,6 +1,6 @@
 import { memo } from 'react'; // memo is HOC from React
 
-// encapsulate whole Reacy component func iside memo func
+// encapsulate whole React component func iside memo func
 // this HOC takes another function as an argument
 // passing ProfileCard function gets us memorized function which we export
 const MemoizedCard = memo(function ProfileCard({ name }) {
