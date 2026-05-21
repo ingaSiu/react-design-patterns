59c93e1422fca4b9c47d229d7d7cff2e507830cf
ingaSiu <ingasiud@gmail.com>
2026-05-21 06:18:13 +0300

    wrap context around components

--- day-13/src/App.jsx
+++ day-13/src/App.jsx
@@ -1,15 +1,13 @@
+import { AuthProvider } from './provider/AuthProvider';
+import LoginButton from './components/LoginButton';
+
 function App() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-xl p-4 m-4">Start Coding in React 19</h1>
-      <p className="bg-gray-300 p-2 rounded-sm text-3xl">
-        <a 
-          href="https://www.youtube.com/watch?v=hiiGUjEkzbM&list=PLIJrr73KDmRw-T8bdJn3XxVMbH-zlooKb" 
-          target="_blank"
-          className="underline mx-1">Learn React 19 With Code</a>
-        <span>▶️</span>  
-      </p>
-    </div>
+    <AuthProvider strategy="otp">
+      <LoginButton />
+      {/*<Profile />
+    <Dashboard />*/}
+    </AuthProvider>
   );
 }
 
