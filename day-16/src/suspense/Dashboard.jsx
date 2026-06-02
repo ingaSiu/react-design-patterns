b934f9e13a0203593653d4bedc3d9a752f641f1f
ingaSiu <ingasiud@gmail.com>
2026-06-02 07:17:37 +0300

    create Dashboard with Suspense

---
+++ day-16/src/suspense/Dashboard.jsx
@@ -0,0 +1,35 @@
+import { AnalyticsSkeleton, OrdersSkeleton, ProfileSkeleton } from '../components/Skeletons';
+
+import Analytics from '../components/Analytics';
+import ErrorBoundary from '../components/ErrorBoundary';
+import Orders from '../components/Orders';
+import Profile from '../components/Profile';
+import { Suspense } from 'react';
+
+export default function Dashboard() {
+  return (
+    <div className="m-2">
+      <header>
+        <h1 className="text-5xl mb-12">📊 Dashboard</h1>
+      </header>
+
+      <ErrorBoundary>
+        <Suspense fallback={<ProfileSkeleton />}>
+          <Profile />
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Suspense fallback={<OrdersSkeleton />}>
+          <Orders />
+        </Suspense>
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Suspense fallback={<AnalyticsSkeleton />}>
+          <Analytics />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  );
+}
