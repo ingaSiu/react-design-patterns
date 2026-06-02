64e322adf0c0be313f3d2129a28bc60e955f6896
ingaSiu <ingasiud@gmail.com>
2026-06-02 07:17:10 +0300

    call createUserResources() then app starts

--- day-16/src/main.jsx
+++ day-16/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createUserResources } from './resources/userResource.js';
+
+createUserResources(); // then app is coming up this will be called and all the promises will be ready
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
