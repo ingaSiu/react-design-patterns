ed2789bcb276491bca666339058d0a7922cc0023
ingaSiu <ingasiud@gmail.com>
2026-06-02 07:17:22 +0300

    comment about use()

--- day-16/src/components/Profile.jsx
+++ day-16/src/components/Profile.jsx
@@ -1,6 +1,9 @@
 import { getUserResources } from '../resources/userResource';
 import { use } from 'react';
 
+// use => can take a Promise as a parameter and whenever the promise value is resolved
+//  it can return data from that promise
+// if promise gets rejected, it returns rejected promise (get error to handle)
 export default function Profile() {
   const { userPromise } = getUserResources();
   const user = use(userPromise);
