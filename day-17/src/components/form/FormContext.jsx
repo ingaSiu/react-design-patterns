522cf3d2166e0e5f2460ab9e60d8f39637f0bf80
ingaSiu <ingasiud@gmail.com>
2026-06-11 07:15:59 +0300

    create form context

---
+++ day-17/src/components/form/FormContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from 'react';
+
+// Create context for form state
+export const FormContext = createContext(null);
+
+// Hook to access form context
+export const useFormContext = () => {
+  // make this context available anythere
+  const context = useContext(FormContext);
+  if (!context) {
+    throw new Error('Form components must be used within a Form component');
+  }
+  return context;
+};
