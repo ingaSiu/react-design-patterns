afcca2696349ba79c3a2804224cc46986e01982f
ingaSiu <ingasiud@gmail.com>
2026-06-13 06:27:15 +0300

    export and import compond pattern components

---
+++ day-17/src/components/form/index.js
@@ -0,0 +1,16 @@
+import { FormButtons } from './FormButtons';
+import { FormField } from './FormField';
+import { Form as FormRoot } from './Form';
+import { FormSelect } from './FormSelect';
+import { FormTextarea } from './FormTextarea';
+
+// Compound component pattern - attach sub-components to main Form
+export const Form = Object.assign(FormRoot, {
+  Field: FormField,
+  Textarea: FormTextarea,
+  Select: FormSelect,
+  Buttons: FormButtons,
+});
+
+// Also export individually if needed
+export { FormButtons, FormField, FormSelect, FormTextarea };
